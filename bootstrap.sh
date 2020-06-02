93304e130d34e0a7e1a8d5190cda122f98181be3
LeptoFlare <leptoflare@gmail.com>
2020-06-02 16:54:20 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,12 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
+  curl -fsSL https://starship.rs/install.sh | bash;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
